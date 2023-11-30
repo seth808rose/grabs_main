e0328a8e4126bb5f350dc6a1771d1b474d976c64
Seta Rose <Seta.Rose@scorpion.co>
2023-11-29 20:40:15 -0800

    added packages, created components

--- grabs/src/App.js
+++ grabs/src/App.js
@@ -1,7 +1,11 @@
+import React from "react";
+import { Home } from "./pages";
+
 function App() {
   return (
     <div className='App'>
       <header>Hello World</header>
+      <Home />
     </div>
   );
 }
---
+++ grabs/src/pages/index.js
@@ -0,0 +1,11 @@
+import Home from "./HomePage";
+// import Cart from "./CartPage";
+// import Checkout from "./CheckoutPage";
+// import About from "./AboutPage";
+// import AuthWrapper from "./AuthWrapper";
+// import Error from "./ErrorPage";
+// import PrivateRoute from "./PrivateRoute";
+// import Products from "./ProductsPage";
+// import SingleProduct from "./SingleProductPage";
+
+export { Home };
